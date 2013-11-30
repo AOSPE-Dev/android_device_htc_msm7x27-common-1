8d74479c406bcc480aeee3632c10674aec66b79e
Kevindeving <98kevchen@gmail.com>
2013-11-30 14:31:20 +0100

    Disable camera again

--- msm7x27.mk
+++ msm7x27.mk
@@ -77,9 +77,9 @@ PRODUCT_PACKAGES += \
     libtilerenderer
 
 # Camera
-#PRODUCT_PACKAGES += \
-#    camera.msm7x27 \
-#    libcamera
+PRODUCT_PACKAGES += \
+    camera.msm7x27 \
+    libcamera
 
 # QCOM OMX
 PRODUCT_PACKAGES += \

e3eb8c0a8f0aa4f481679fb2b221059f39e8a228
thehacka1 <kalakotig@gmail.com>
2013-12-26 10:43:43 +0000

    Update BoardConfigCommon.mk

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -12,29 +12,39 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-TARGET_SPECIFIC_HEADER_PATH := device/htc/msm7x27-common/include
+TARGET_SPECIFIC_HEADER_PATH := device/htc/marvel/msm7x27-common/include
 
 # Make sure this folder exists so display stuff doesn't fail
 $(shell mkdir -p $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/)
 
 # inherit from common msm7x27 Recovery
--include device/htc/msm7x27-common/recovery/BoardConfigCommon.mk
+-include device/htc/marvel/msm7x27-common/recovery/BoardConfigCommon.mk
 
+
+# CPU
 TARGET_ARCH := arm
 TARGET_CPU_ABI := armeabi-v6l
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv6-vfp
-TARGET_CPU_VARIANT := arm11
 TARGET_BOARD_PLATFORM := msm7x27
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+
+# CFLAGS
 COMMON_GLOBAL_CFLAGS += -DTARGET_MSM7x27
+COMMON_GLOBAL_CFLAGS += -DREFRESH_RATE=60
+COMMON_GLOBAL_CFLAGS += -DTARGET_MSM7x27
+COMMON_GLOBAL_CFLAGS += -DQCOM_LEGACY_OMX
+COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE
+COMMON_GLOBAL_CFLAGS += -DQCOM_NO_SECURE_PLAYBACK -DANCIENT_GL
+COMMON_GLOBAL_CFLAGS += -DBINDER_COMPAT
+COMMON_GLOBAL_CFLAGS += -DMISSING_GRALLOC_BUFFERS
+COMMON_GLOBAL_CFLAGS += -DQCOM_ICS_COMPAT
+COMMON_GLOBAL_CFLAGS += -DQCOM_MISSING_PIXEL_FORMATS
+COMMON_GLOBAL_CFLAGS += -DBINDER_COMPAT
 
 ### Board config
 TARGET_NO_BOOTLOADER := true
 TARGET_NO_RADIOIMAGE := true
-
-TARGET_PROVIDES_LIBLIGHT := true
-TARGET_PROVIDES_LIBAUDIO := true
 
 ### Kernel
 TARGET_KERNEL_SOURCE := kernel/htc/msm7227
@@ -54,19 +64,32 @@ WIFI_DRIVER_MODULE_ARG := "firmware_path=/system/etc/firmware/fw_bcm4329.bin nvr
 WIFI_DRIVER_MODULE_NAME := "bcm4329"
 WIFI_DRIVER_FW_PATH_STA := "/system/etc/firmware/fw_bcm4329.bin"
 WIFI_DRIVER_FW_PATH_AP := "/system/etc/firmware/fw_bcm4329_apsta.bin"
-#WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/bcm4329/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/bcm4329/parameters/firmware_path"
 BOARD_WLAN_DEVICE_REV := bcm4329
 WIFI_BAND := 802_11_ABG
 
-### Graphics
-# Option for choosing texture target on old adrenos
-BOARD_ADRENO_DECIDE_TEXTURE_TARGET := true
-BOARD_EGL_CFG := device/htc/msm7x27-common/egl.cfg
-TARGET_QCOM_DISPLAY_VARIANT := legacy
-BOARD_EGL_WORKAROUND_BUG_10194508 := true
+### Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+BOARD_BLUETOOTH_USES_HCIATTACH_PROPERTY := false
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/htc/marvel/msm7x27-common/bluetooth/include
+BOARD_BLUEDROID_VENDOR_CONF := device/htc/marvel/msm7x27-common/bluetooth/vnd_msm7x27.txt
 
-COMMON_GLOBAL_CFLAGS += -DREFRESH_RATE=60 -DANCIENT_GL
+### QCOM
+BOARD_USES_QCOM_HARDWARE := true
+BOARD_USES_QCOM_LIBS := true
+BOARD_USES_QCOM_LIBRPC := true
+BOARD_USES_QCOM_GPS := true
+BOARD_USE_QCOM_PMEM := true
+
+# Adreno
+BOARD_ADRENO_DECIDE_TEXTURE_TARGET := true
+BOARD_EGL_CFG := device/htc/marvel/msm7x27-common/egl.cfg
+### CPU/Graphics
+TARGET_QCOM_DISPLAY_VARIANT := legacy
 USE_OPENGL_RENDERER := true
+BOARD_USE_ADRENO_200_GPU := true
+BOARD_ADRENO_DECIDE_TEXTURE_TARGET := true
 TARGET_USES_C2D_COMPOSITION := false
 TARGET_USES_SF_BYPASS := false
 TARGET_HAVE_BYPASS := false
@@ -75,45 +98,33 @@ TARGET_QCOM_HDMI_OUT := false
 TARGET_GRALLOC_USES_ASHMEM := false
 TARGET_USES_GENLOCK := true
 TARGET_NO_HW_VSYNC := true
-COMMON_GLOBAL_CFLAGS += -DQCOM_MISSING_PIXEL_FORMATS
 BOARD_EGL_NEEDS_LEGACY_FB := true
-
-#TARGET_NO_HW_OVERLAY := true
-#BOARD_NEEDS_MEMORYHEAPPMEM := true
-#TARGET_DISABLE_TRIPLE_BUFFERING := true
+BOARD_NO_RGBX_8888 := true
 
 
 ### Camera
-#BOARD_USES_LEGACY_CAMERA := true
+BOARD_CAMERA_5MP := true
 BOARD_USE_NASTY_PTHREAD_CREATE_HACK := true
 BOARD_NEEDS_MEMORYHEAPPMEM := true
-COMMON_GLOBAL_CFLAGS += -DBINDER_COMPAT
-COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
-#TARGET_DISABLE_ARM_PIE := true
+USE_CAMERA_STUB := false
 
 ### Audio
 BOARD_USES_GENERIC_AUDIO := false
 BOARD_PREBUILT_LIBAUDIO := false
 BOARD_USES_QCOM_AUDIO_VOIPMUTE := true
 BOARD_USES_QCOM_AUDIO_RESETALL := true
+BOARD_USES_AUDIO_LEGACY := false
+TARGET_PROVIDES_LIBAUDIO := true
+BOARD_COMBO_DEVICE_SUPPORTED := true
 
-### Bluetooth
-BOARD_HAVE_BLUETOOTH := true
-BOARD_HAVE_BLUETOOTH_BCM := true
-BOARD_BLUETOOTH_USES_HCIATTACH_PROPERTY := false
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/htc/msm7x27-common/bluetooth/include
-BOARD_BLUEDROID_VENDOR_CONF := device/htc/msm7x27-common/bluetooth/vnd_msm7x27.txt
+### Lights
+TARGET_PROVIDES_LIBLIGHT := true
 
-### Boot animation
+### Bootanimation
 TARGET_BOOTANIMATION_USE_RGB565 := true
 TARGET_SCREEN_HEIGHT := 480
 TARGET_SCREEN_WIDTH := 320
 
-### QCOM
-BOARD_USES_QCOM_LIBS := true
-BOARD_USES_QCOM_LIBRPC := true
-BOARD_USES_QCOM_GPS := true
-BOARD_USE_QCOM_PMEM := true
 
 ###GPS moved here temporarily
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := msm7x27
@@ -127,18 +138,14 @@ BOARD_USE_NEW_LIBRIL_HTC := true
 SMALLER_FONT_FOOTPRINT := true
 
 ### Dalvik
-# If WITH_JIT is configured, build multiple versions of libdvm.so to facilitate
-# correctness/performance bugs triage
 WITH_JIT := true
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heaptargetutilization=0.25 \
-    dalvik.vm.jit.codecachesize=0
-	
-### Webkit
-TARGET_FORCE_CPU_UPLOAD := true
+
 
 ### Browser
 JS_ENGINE := v8
 HTTP := chrome
 ENABLE_JSC_JIT := true
 ENABLE_WEBGL := true
+
+### Webkit
+TARGET_FORCE_CPU_UPLOAD := true
